90d00a418e36f3e348078656790fb44d1b266dc9
Paul Siersma <paul.siersma@gmail.com>
2015-12-07 21:50:44 +0100

    Finished ValidationService tests

--- src/SolidFrame.Validation.Test/ValidationServiceTests.cs
+++ src/SolidFrame.Validation.Test/ValidationServiceTests.cs
@@ -78,6 +78,104 @@ namespace SolidFrame.Validation.Test
 	}
 
 	[TestFixture]
+	public class DescribeAbsoluteRuleAddition
+	{
+		private IValidationService<CanBeValidatedStub> _validationService;
+		private Mock<INotificationService> _notificationServiceMock;
+		private Mock<IValidationRule<CanBeValidatedStub>> _validationRuleMock;
+		private Mock<IPropertyNameHelper> _propertyNameHelperMock;
+		private string _message;
+		private Mock<IConditionEvaluatorFactory> _conditionEvaluatorFactoryMock;
+		private Mock<IValidationRuleFactory> _validationRuleFactoryMock;
+		private Mock<IConditionEvaluator<CanBeValidatedStub>> _greaterThanEvaluatorMock;
+		private Func<CanBeValidatedStub, int> _getter1;
+		private Func<CanBeValidatedStub, int> _getter2;
+
+
+		[SetUp]
+		public void BeforeEach()
+		{
+			var validationServiceDependenciesMock = new Mock<IValidationServiceDependencies>();
+
+			_propertyNameHelperMock = new Mock<IPropertyNameHelper>();
+			_propertyNameHelperMock.Setup(h => h.GetPropertyName(It.IsAny<Expression<Func<CanBeValidatedStub, int>>>()))
+				.Returns<Expression<Func<CanBeValidatedStub, int>>>(e => new PropertyNameHelper().GetPropertyName(e)).Verifiable();
+
+			validationServiceDependenciesMock.SetupGet(v => v.PropertyNameHelper).Returns(_propertyNameHelperMock.Object);
+
+			_greaterThanEvaluatorMock = new Mock<IConditionEvaluator<CanBeValidatedStub>>();
+
+			_conditionEvaluatorFactoryMock = new Mock<IConditionEvaluatorFactory>();
+			_conditionEvaluatorFactoryMock.Setup(
+				f =>
+					f.CreateGreaterThanEvaluator(It.IsAny<Func<CanBeValidatedStub, int>>(), It.IsAny<Func<CanBeValidatedStub, int>>()))
+				.Returns(_greaterThanEvaluatorMock.Object)
+				.Callback<Func<CanBeValidatedStub, int>, Func<CanBeValidatedStub, int>>((func, func1) =>
+				{
+					_getter1 = func;
+					_getter2 = func1;
+				});
+
+			validationServiceDependenciesMock.SetupGet(d => d.ConditionEvaluatorFactory)
+				.Returns(_conditionEvaluatorFactoryMock.Object);
+
+			_validationRuleMock = new Mock<IValidationRule<CanBeValidatedStub>>();
+			_validationRuleMock.Setup(r => r.Evaluate(It.IsAny<CanBeValidatedStub>())).Returns(true);
+			_validationRuleMock.SetupGet(r => r.Properties).Returns(new HashSet<string>()).Verifiable();
+
+			_validationRuleFactoryMock = new Mock<IValidationRuleFactory>();
+			_validationRuleFactoryMock.Setup(
+				f =>
+					f.Create(It.IsAny<IConditionEvaluator<CanBeValidatedStub>>(), It.IsAny<Severity>(), It.IsAny<string>(),
+						It.IsAny<string>()))
+				.Returns(_validationRuleMock.Object).Callback<IConditionEvaluator<CanBeValidatedStub>, Severity, string, string[]>((evaluator, severity, arg3, arg4) => _message = arg3);
+
+			validationServiceDependenciesMock.SetupGet(d => d.ValidationRuleFactory).Returns(_validationRuleFactoryMock.Object);
+
+			_notificationServiceMock = new Mock<INotificationService>();
+			_notificationServiceMock.Setup(s => s.TryRemoveNotification(It.IsAny<Guid>(), It.IsAny<Guid>())).Verifiable();
+
+			_validationService = new ValidationService<CanBeValidatedStub>(validationServiceDependenciesMock.Object);
+
+			var validateMock = new Mock<IValidate<CanBeValidatedStub>>();
+
+			_validationService.Register(validateMock.Object);
+
+			_validationService.AddAbsoluteRule(new Mock<IHaveId>().Object, stub => stub.NumberInt, Condition.MustBeGreaterThan, 0, Severity.Error, "Test{0}");
+		}
+
+		[Test]
+		public void It_gets_propertyName_from_Expression()
+		{
+			// Verify call to the PropertyNameHelper
+			_propertyNameHelperMock.Verify(v => v.GetPropertyName<CanBeValidatedStub, int>(stub => stub.NumberInt));
+		}
+
+		[Test]
+		public void It_gets_ConditionEvaluator_from_the_factory()
+		{
+			_conditionEvaluatorFactoryMock.Verify(f => f.CreateGreaterThanEvaluator(It.IsAny<Func<CanBeValidatedStub, int>>(), It.IsAny<Func<CanBeValidatedStub, int>>()), Times.Once);
+
+			Assert.AreEqual(1, _getter1(new CanBeValidatedStub{NumberInt = 1}));
+			Assert.AreEqual(0, _getter2(new CanBeValidatedStub()));
+		}
+
+		[Test]
+		public void It_gets_the_ValidationRule_from_the_IValidationRuleFactory()
+		{
+			// Verify call to the ValidationRuleFactory with the right arguments
+			_validationRuleFactoryMock.Setup(
+				f => f.Create(_greaterThanEvaluatorMock.Object, Severity.Error, _message, "NumberInt"));
+		}
+
+		[Test]
+		public void It_creates_a_formatted_validationmessage()
+		{
+			Assert.AreEqual("TestNumberInt", _message);
+		}
+	}
+
+	[TestFixture]
 	public class DescribeRuleEvaluation
 	{
 		private IValidationService<CanBeValidatedStub> _validationService;
@@ -130,6 +228,8 @@ namespace SolidFrame.Validation.Test
 			_notificationServiceMock.Setup(s => s.TryRemoveNotification(It.IsAny<Guid>(), It.IsAny<Guid>())).Verifiable();
 			_notificationServiceMock.Setup(s => s.AddNotification(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<string>())).Verifiable();
 
+			validationServiceDependenciesMock.SetupGet(d => d.NotificationService).Returns(_notificationServiceMock.Object);
+
 			_validationService = new ValidationService<CanBeValidatedStub>(validationServiceDependenciesMock.Object);
 
 			_validateMock = new Mock<IValidate<CanBeValidatedStub>>();
@@ -159,43 +259,18 @@ namespace SolidFrame.Validation.Test
 
 			// Test that AddNotification is called with right args
 			_notificationServiceMock.Verify(s => s.AddNotification(_validationRuleId, stub.Id, validationName, _validationRuleMessage));
-
-			Assert.Fail("Please implement");
 		}
 
 		[Test]
 		public void It__tries_to_remove_Rule_Notification_if_Rule_evaluates_to_true()
 		{
 			_validationRuleMock.Setup(r => r.Evaluate(It.IsAny<CanBeValidatedStub>())).Returns(true);
-	
-			_validateMock.Raise(v => v.RowValidationTrigger += null, new CanBeValidatedStub(), "NumberInt");
 
-			// Test that TryRemoveNotification is called with right args
+			var stub = new CanBeValidatedStub();
 
-			Assert.Fail("Please implement");
-		}
-	}
+			_validateMock.Raise(v => v.RowValidationTrigger += null, stub, "NumberInt");
 
-	public class DescribeAbsoluteRuleAddition
-	{
-		public void It_gets_propertyName_from_Expression()
-		{
-			
-		}
-
-		public void It_gets_ConditionEvaluator_from_the_factory()
-		{
-			
-		}
-
-		public void It_gets_the_ValidationRule_from_the_IValidationRuleFactory()
-		{
-			
-		}
-
-		public void It_evaluates_added_rules_when_triggered_to()
-		{
-			
+			_notificationServiceMock.Verify(s => s.TryRemoveNotification(_validationRuleId, stub.Id));
 		}
 	}
 }
